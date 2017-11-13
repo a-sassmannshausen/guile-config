57dc24784996de930472f627df763227eab17563
Alex Sassmannshausen <alex@pompo.co>
2017-11-13 16:47:52 +0100

    sexp-parser: Make it work.
    * config/parser/sexp.scm (sexp-reader): Accept configuration as param.
      (sexp-writer): Fix document writing.

--- config/parser/sexp.scm
+++ config/parser/sexp.scm
@@ -44,7 +44,7 @@
 ;;     consider a parser that writes multiple configuration files.
 ;; - The reader simply reads the specific subcommand of the file.
 
-(define (sexp-reader)
+(define (sexp-reader configuration)
   #t)
 
 (define* (sexp-writer configuration #:optional port)
@@ -60,29 +60,33 @@
                               ((? string? s) s))
                             file-name-separator-string
                             "config.scm") <>)))
-  (with-output-proc
-   (lambda ()
-     (print
-      (let lp ((forest (list configuration))
-               (rev-breadcrumbs '()))
-        (fold (lambda (current result)
-                (append result
-                        (let ((rev-breadcrumbs (cons (configuration-name current)
-                                                     rev-breadcrumbs)))
-                          (cons `((breadcrumbs . ,(reverse rev-breadcrumbs))
-                                  (help . ,(match (configuration-description current)
-                                             ((? string-null?)
-                                              (configuration-synopsis current))
-                                             (desc desc)))
-                                  (settings . ,(keywords->setting-specs
-                                                (configuration-keywords current))))
+  (match (configuration-directory configuration)
+    (($ <empty>) #t)
+    ((? string? dirname)
+     (mkdir-p (configuration-directory configuration))
+     (with-output-proc
+      (lambda ()
+        (print
+         (let lp ((forest (list configuration))
+                  (rev-breadcrumbs '()))
+           (fold (lambda (current result)
+                   (append result
+                           (let ((rev-breadcrumbs (cons (configuration-name current)
+                                                        rev-breadcrumbs)))
+                             (cons `((breadcrumbs . ,(reverse rev-breadcrumbs))
+                                     (help . ,(match (configuration-description current)
+                                                ((? string-null?)
+                                                 (configuration-synopsis current))
+                                                (desc desc)))
+                                     (settings . ,(keywords->setting-specs
+                                                   (configuration-keywords current))))
 
-                                 (match (configuration-subcommands current)
-                                   (() '())
-                                   (subcommands
-                                    (lp subcommands rev-breadcrumbs)))))))
-              '()
-              forest))))))
+                                   (match (configuration-subcommands current)
+                                     (() '())
+                                     (subcommands
+                                      (lp subcommands rev-breadcrumbs)))))))
+                 '()
+                 forest))))))))
 
 (define sexp-parser
   (make-parser sexp-reader sexp-writer))
@@ -114,7 +118,7 @@
 ;;;; Helpers
 
 (define* (print printables #:optional (port (current-output-port)))
-  ;; (pretty-print printables)
+  (pretty-print printables)
   (for-each
    (match-lambda
      ((('breadcrumbs . breadcrumbs)
@@ -164,118 +168,118 @@
 
 ;; help entries are either description or synopsis (preference for
 ;; description)
-(print '(((name . (application-name))
-          (help . "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
+;; (print '(((name . (application-name))
+;;           (help . "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
 
-We also want to make sure that it looks alright when we have:
+;; We also want to make sure that it looks alright when we have:
 
-- multiple paragraphs
+;; - multiple paragraphs
 
-- a list somewhere in the middle
+;; - a list somewhere in the middle
 
-And finally a closing sentence")
-          (settings . (((name . option-name)
-                        (help . "This is the long description for this setting. Like above, the same tests apply:
+;; And finally a closing sentence")
+;;           (settings . (((name . option-name)
+;;                         (help . "This is the long description for this setting. Like above, the same tests apply:
 
-New paragraph.
+;; New paragraph.
 
-- perhaps a list.
+;; - perhaps a list.
 
-Then some more text.")
-                        (value . 43)))))
-         ((name . (application-name subcommand-1))
-          (help . "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
+;; Then some more text.")
+;;                         (value . 43)))))
+;;          ((name . (application-name subcommand-1))
+;;           (help . "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
 
-We also want to make sure that it looks alright when we have:
+;; We also want to make sure that it looks alright when we have:
 
-- multiple paragraphs
+;; - multiple paragraphs
 
-- a list somewhere in the middle
+;; - a list somewhere in the middle
 
-And finally a closing sentence")
-          (settings . (((name . option-name-subcommand-1)
-                        (help . "This is the long description for this setting. Like above, the same tests apply:
+;; And finally a closing sentence")
+;;           (settings . (((name . option-name-subcommand-1)
+;;                         (help . "This is the long description for this setting. Like above, the same tests apply:
 
-New paragraph.
+;; New paragraph.
 
-- perhaps a list.
+;; - perhaps a list.
 
-Then some more text.")
-                        (value . 43)))))))
+;; Then some more text.")
+;;                         (value . 43)))))))
 
 
-(sexp-writer
- (configuration
-  (name 'test-commander)
-  (parser identity-parser)
-  (description
-   "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
+;; (sexp-writer
+;;  (configuration
+;;   (name 'test-commander)
+;;   (parser identity-parser)
+;;   (description
+;;    "This is a long description of what this command does.  The text will go on for a bit and we will at some point need to use fill-paragraph to preformat this shit.  But we probably want to do that in print's caller.
 
-We also want to make sure that it looks alright when we have:
+;; We also want to make sure that it looks alright when we have:
 
-- multiple paragraphs
+;; - multiple paragraphs
 
-- a list somewhere in the middle
+;; - a list somewhere in the middle
 
-And finally a closing sentence")
-  (synopsis "Should not display")
-  (keywords
-   (list (setting
-          (name 'verbose)
-          (default #f)
-          (test boolean?)
-          (synopsis "Should we emit more logging info?"))
-         (switch
-          (name 'uninstall)
-          (default #f)
-          (inheritable? #f)))))
- (current-output-port))
+;; And finally a closing sentence")
+;;   (synopsis "Should not display")
+;;   (keywords
+;;    (list (setting
+;;           (name 'verbose)
+;;           (default #f)
+;;           (test boolean?)
+;;           (synopsis "Should we emit more logging info?"))
+;;          (switch
+;;           (name 'uninstall)
+;;           (default #f)
+;;           (inheritable? #f)))))
+;;  (current-output-port))
 
-(sexp-writer
- (configuration
-   (name 'test-commander)
-   (parser identity-parser)
-   (copyright '(2016 2018))
-   (author "Alex Sassmannshausen")
-   (synopsis "Test Configuration")
-   (description
-    "This application definition merely shows what we are capable of.")
-   (keywords
-    (list (setting
-           (name 'verbose)
-           (default #f)
-           (test boolean?)
-           (synopsis "Should we emit more logging info?"))
-          (switch
-           (name 'uninstall)
-           (default #f)
-           (inheritable? #f))))
-   (subcommands
-    (list (configuration (name 'child1)
-                         (synopsis "First child"))
-          (configuration (name 'child2)
-                         (synopsis "Second child")
-                         (subcommands
-                          (list (configuration (name 'childchild2)))))
-          (configuration (name 'child3)
-                         (alias 'c3)
-                         (keywords
-                          (list (setting
-                                 (name 're-uninstall)
-                                 (synopsis "Should we re-uninstall?")
-                                 (default #f)
-                                 (test boolean?))
-                                (setting
-                                 (name 'path)
-                                 (default "$HOME")
-                                 (synopsis "The path.")
-                                 (example "/home/alice/.config"))))
-                         (subcommands
-                          (list (configuration
-                                 (name 'grandchild1)
-                                 (alias 'gc1)
-                                 (subcommands
-                                  (list (configuration (name 'grandchildchild1)))))
-                                (configuration (name 'grandchild2) (alias 'gc2)))))
-          (configuration (name 'child4)))))
- (current-output-port))
+;; (sexp-writer
+;;  (configuration
+;;    (name 'test-commander)
+;;    (parser identity-parser)
+;;    (copyright '(2016 2018))
+;;    (author "Alex Sassmannshausen")
+;;    (synopsis "Test Configuration")
+;;    (description
+;;     "This application definition merely shows what we are capable of.")
+;;    (keywords
+;;     (list (setting
+;;            (name 'verbose)
+;;            (default #f)
+;;            (test boolean?)
+;;            (synopsis "Should we emit more logging info?"))
+;;           (switch
+;;            (name 'uninstall)
+;;            (default #f)
+;;            (inheritable? #f))))
+;;    (subcommands
+;;     (list (configuration (name 'child1)
+;;                          (synopsis "First child"))
+;;           (configuration (name 'child2)
+;;                          (synopsis "Second child")
+;;                          (subcommands
+;;                           (list (configuration (name 'childchild2)))))
+;;           (configuration (name 'child3)
+;;                          (alias 'c3)
+;;                          (keywords
+;;                           (list (setting
+;;                                  (name 're-uninstall)
+;;                                  (synopsis "Should we re-uninstall?")
+;;                                  (default #f)
+;;                                  (test boolean?))
+;;                                 (setting
+;;                                  (name 'path)
+;;                                  (default "$HOME")
+;;                                  (synopsis "The path.")
+;;                                  (example "/home/alice/.config"))))
+;;                          (subcommands
+;;                           (list (configuration
+;;                                  (name 'grandchild1)
+;;                                  (alias 'gc1)
+;;                                  (subcommands
+;;                                   (list (configuration (name 'grandchildchild1)))))
+;;                                 (configuration (name 'grandchild2) (alias 'gc2)))))
+;;           (configuration (name 'child4)))))
+;;  (current-output-port))
